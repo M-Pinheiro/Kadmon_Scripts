31b130569948e3e8aa423771309925fa2778ee31
M-Pinheiro <marco.pinheiro@student.manchester.ac.uk>
2014-04-01 14:00:59 +0100

    Transcriptome Aligment edit of path to refseq
    edited of path to the reference transcriptome

--- Transcriptome_Alignment.sh
+++ Transcriptome_Alignment.sh
@@ -12,5 +12,5 @@ tar -zcvf SRR767626.sra.tar.gz SRR767626.sra
 
 load module apps/bowtie2/2.2.1
 
-bowtie2 -q -k 100 -N 3 -S SRR767606.sam --no-discordant  RefSeq_Genes -1 SRR767606_1.fastq -2 SRR767606_2.fastq
-bowtie2 -q -k 100 -N 3 -S SRR767626.sam --no-discordant  RefSeq_Genes -1 SRR767626_1.fastq -2 SRR767626_2.fastq
+bowtie2 -q -k 100 -N 3 -S SRR767606.sam --no-discordant  /mnt/fls01-home01/mfbx9mp5/scratch/Rotation_2/RefSeq_genes -1 SRR767606_1.fastq -2 SRR767606_2.fastq
+bowtie2 -q -k 100 -N 3 -S SRR767626.sam --no-discordant  /mnt/fls01-home01/mfbx9mp5/scratch/Rotation_2/RefSeq_genes -1 SRR767626_1.fastq -2 SRR767626_2.fastq
